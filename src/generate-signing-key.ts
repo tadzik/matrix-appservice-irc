515c09d155f8569eaa4066c002350a1fb44aa0a0
Tadeusz „tadzik” Sośnierz <tadeusz@sosnierz.com>
2024-08-14 12:30:33 +0200

    Make singing key generator emit valid JSON; include a sample in test files

--- src/generate-signing-key.ts
+++ src/generate-signing-key.ts
@@ -5,7 +5,7 @@ async function main() {
         name: 'HMAC',
         hash: 'SHA-512',
     }, true, ['sign', 'verify']);
-    console.log(await webcrypto.subtle.exportKey('jwk', key));
+    console.log(JSON.stringify(await webcrypto.subtle.exportKey('jwk', key), undefined, 4));
 }
 
 main().then(() => process.exit(0)).catch(err => { throw err });
